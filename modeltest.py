aa7a6e58d0b86cab9ec92f130bcd7f796139c506
nicey <vuminhanh.dang@student.unimelb.edu.au>
2025-05-07 14:49:58 +1000

    refactor regression models to a separate function

--- modeltest.py
+++ modeltest.py
@@ -24,8 +24,6 @@ from sklearn.model_selection import RandomizedSearchCV
 
 # print the table that we want to a .csv file
 def custom_compare_models(df):
-    # Evaluate if the data is balanced or imbalanced
-
     # Scaling?
     
     train_X = df.drop("SEVERITY", axis=1) # !reminder, classification and regression models take different SEVERITY colums
@@ -50,72 +48,10 @@ def custom_compare_models(df):
             "columns": all_cols
         }
     }
-    
 
-    ######### REGRESSON MODELS #########
-    # Models Initialisation
-    lin_reg = LinearRegression()
-    tree_reg = DecisionTreeRegressor()
-    forest_reg = RandomForestRegressor()
-    
-    # Hyperparameters for fine tuning
-    models_configs = {
-        0 : {
-            "name": 'Linear Regressor',
-            "model": lin_reg,
-            "param_grid": {
-                "fit_intercept": [True],
-                "copy_X": [True]
-                }
-        },
-        1 : {
-            "name": "Decision Tree Regressor",
-            "model": tree_reg,
-            "param_grid": {
-                "criterion": ["gini", "entropy"], # ??????????
-                "max_depth": [3, None],
-                "min_samples_leaf": randint(1, 9),
-                "max_features": randint(1, 9)
-                }
-        },
-        2 : {
-            "name": "Random Forest Regressor",
-            "model": forest_reg,
-            "param_grid": {
-                'n_estimators': [130, 180, 230],
-                'max_depth': [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, None],
-                'min_samples_split': [2, 5, 10],
-                'min_samples_leaf': [1, 2, 4],
-                'max_features': ['auto', 'sqrt'],
-                'bootstrap': [True, False]
-                }
-        }
-    }
-    
-    # scorings = {'accuracy': make_scorer(accuracy_score),
-    #            'prec': 'precision'}
+    compare_classification_models(df, variations)
+    compare_regression_models(df, variations)
 
-    # Evaluation
-    # for each variation
-    for i in range(0, 3):
-        print(variations[i]['name'])
-        X_cols = train_X.copy()[variations[i]['columns']]
-        # for each model
-        for j in range(0, 3):
-            # Fine tuning
-            rand_search = RandomizedSearchCV(models_configs[j]["model"], models_configs[j]["param_grid"], cv=10,
-                                             scoring='neg_mean_squared_error', # forcing to negative to make it easier to sqrt
-                                             return_train_score=True)
-            rand_search.fit(X_cols, train_Y)
-
-            # print (hyperparameters, RMSE) of the best model
-            print(models_configs[j]["name"])
-            eval_res = pd.DataFrame(rand_search.cv_results_)
-            param_res = pd.DataFrame(rand_search.best_params_)
-            res_df = pd.concat([eval_res, param_res])
-            print(res_df.head())
-            # for mean_score, params in zip(eval_res["mean_test_score"], eval_res["params"]):
-            #     print(np.sqrt(-mean_score), params) # printing for now, change to writing to csv later
 
 # Takes a dataframe with all x values, but y values with labels only
 def compare_classification_models(df, variations):
@@ -194,3 +130,72 @@ def imbalanced_evaluate(test_Y, pred_y):
     precidion_dt = round(precision_score(test_Y, pred_y, average='weighted'),2)
     f1_dt = round(f1_score(test_Y, pred_y,average='weighted'),2)
     return recall_dt, precidion_dt, f1_dt
+
+# Takes a dataframe with all x values, but numerical y values only
+def compare_regression_models(df, variations):
+    train_X = df.drop("SEVERITY", axis=1) # !reminder, classification and regression models take different SEVERITY colums
+    train_Y = df["SEVERITY"].copy()
+
+    # Models Initialisation
+    lin_reg = LinearRegression()
+    tree_reg = DecisionTreeRegressor()
+    forest_reg = RandomForestRegressor()
+    
+    # Hyperparameters for fine tuning
+    models_configs = {
+        0 : {
+            "name": 'Linear Regressor',
+            "model": lin_reg,
+            "param_grid": {
+                "fit_intercept": [True],
+                "copy_X": [True]
+                }
+        },
+        1 : {
+            "name": "Decision Tree Regressor",
+            "model": tree_reg,
+            "param_grid": {
+                "criterion": ["gini", "entropy"], # ??????????
+                "max_depth": [3, None],
+                "min_samples_leaf": randint(1, 9),
+                "max_features": randint(1, 9)
+                }
+        },
+        2 : {
+            "name": "Random Forest Regressor",
+            "model": forest_reg,
+            "param_grid": {
+                'n_estimators': [130, 180, 230],
+                'max_depth': [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, None],
+                'min_samples_split': [2, 5, 10],
+                'min_samples_leaf': [1, 2, 4],
+                'max_features': ['auto', 'sqrt'],
+                'bootstrap': [True, False]
+                }
+        }
+    }
+    
+    # scorings = {'accuracy': make_scorer(accuracy_score),
+    #            'prec': 'precision'}
+
+    # Evaluation
+    # for each variation
+    for i in range(0, 3):
+        print(variations[i]['name'])
+        X_cols = train_X.copy()[variations[i]['columns']]
+        # for each model
+        for j in range(0, 3):
+            # Fine tuning
+            rand_search = RandomizedSearchCV(models_configs[j]["model"], models_configs[j]["param_grid"], cv=10,
+                                             scoring='neg_mean_squared_error', # forcing to negative to make it easier to sqrt
+                                             return_train_score=True)
+            rand_search.fit(X_cols, train_Y)
+
+            # print (hyperparameters, RMSE) of the best model
+            print(models_configs[j]["name"])
+            eval_res = pd.DataFrame(rand_search.cv_results_)
+            param_res = pd.DataFrame(rand_search.best_params_)
+            res_df = pd.concat([eval_res, param_res])
+            print(res_df.head())
+            # for mean_score, params in zip(eval_res["mean_test_score"], eval_res["params"]):
+            #     print(np.sqrt(-mean_score), params) # printing for now, change to writing to csv later
